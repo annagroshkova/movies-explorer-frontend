2b360cf2ff85c3f3dffe1f222c00f3c49390b380
Anna Groshkova <anna.matvyeyenko@gmail.com>
2023-07-09 23:14:02 +0200

    Movies container done

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Header from "./Header"
 import SearchBar from "./SearchBar";
+import MoviesCardList from "./MoviesCardList";
+import Footer from "./Footer";
 
 export default function Main() {
   return (
   <div className="main">
     <Header />
     <SearchBar />
+    <MoviesCardList />
+    <Footer />
   </div>
   )
 }
