23b0884011d17bfcaa97e4e8adc0a10b7ea5fcde
Anna Groshkova <anna.matvyeyenko@gmail.com>
2023-08-09 13:52:30 +0200

    Component NotFound responsive

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from "react"
 import "./NotFound.css";
+import {Link} from "react-router-dom";
 
 export default function NotFound() {
   return (
   <div className="not-found">
-    404
+    <h1 className="not-found__title">404</h1>
+    <div className="not-found__text">
+      <p className="not-found__subtitle">Страница не найдена</p>
+      <Link className="not-found__link" to="/">Назад</Link>
+    </div>
   </div>
   )
 }
